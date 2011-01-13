bb53f77ebc3eeb6b441d3f1382bbf11ccacfe2ca
orta <orta.therox@gmail.com>
2011-01-13 19:35:04 +0000

    can post to the DB

--- app.rb
+++ app.rb
@@ -24,6 +24,30 @@ get '/' do
   haml :index
 end
 
+get '/create' do
+  haml :create
+end
+
+post '/create' do
+  
+  doc_url = "#{DB}/#{params[:permalink]}"
+  rev = JSON.parse( RestClient.get( doc_url ) )['_rev'] rescue nil
+
+  new_doc = {
+    'date' => params[:date],
+    'style' => params[:style],
+    'message' => params[:message]
+  }
+  new_doc['_rev'] = rev if rev
+
+  result = RestClient.put doc_url, new_doc.to_json, :content_type => 'application/json'
+  
+  # redirect!
+  redirect '/' + params[:permalink]
+  
+  haml :contact
+end
+
 get '/:permalink' do
   begin 
     @data = RestClient.get "#{DB}/#{params[:permalink]}"
